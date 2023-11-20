1de4e46023fe655983dcc2482451a9360faea76b
Bilel Barhoumi <infobilel@gmail.com>
2023-11-21 00:09:22 +0100

    Adding Bootstrap

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <div class="row align-items-center justify-content-center vh-100 text-center">
+      <h1 class="display-5">This is an about page</h1>
+    </div>
+  </main>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="row align-content-around vh-100">
+      <h1 class="text-center display-5">This is an home page</h1>
+    </div>
   </main>
 </template>
