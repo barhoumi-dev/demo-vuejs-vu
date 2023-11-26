a3f5fc354172a69476a3f47e505ba8e439ddb82a
Bilel Barhoumi <infobilel@gmail.com>
2023-11-26 13:46:55 +0100

    Using private repo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import BTodoList from '@barhoumi-dev/private-package/src/components/BTodoList.vue'
 </script>
 
 <template>
   <main>
-    <div class="row align-content-around vh-100">
+    <div class="row align-content-center vh-100">
       <h1 class="text-center display-5">This is an home page</h1>
+      <BTodoList/>
     </div>
   </main>
 </template>
